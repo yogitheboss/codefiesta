4190fdaa8c31fb9e6f7d2e289bae4bd3339f841d
yogitheboss <yograjrjpt123@gmail.com>
2023-04-15 18:04:18 +0530

    added models route

--- b-eagleeye/Routes/accounts.js
+++ b-eagleeye/Routes/accounts.js
@@ -20,3 +20,27 @@ router.get('/get_account', async (req, res) => {
         })
     }
 })
+// router.post('/get_account', async (req, res) => {
+//     // get all parking details
+//     const details = req.body
+
+//     try {
+//         // if details are found then send response 
+//         account.insertMany(details)
+//         console.log(details);
+//         res.json({
+//             success: true,
+//             details: details
+//         })
+//     } catch (err) {
+//         console.log(err);
+//         res.json({
+//             success: false,
+//             message: "No details found"
+//         })
+//     }
+// })
+
+
+
+module.exports = router;
